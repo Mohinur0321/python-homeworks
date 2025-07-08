277ca0de61a8e63c368fbe66dcbbced1973051f1
Mohinur0321 <mokhinurernazarova890@gmail.com>
2025-07-08 19:09:47 +0500

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,27 @@
+#1
+side = 5
+perimeter = 4 * side
+area = side ** 2
+print(f'Perimeter is {perimeter}, Area is {area}')
+  
+#2
+import math
+diameter = 10
+length_of_circle = math.pi * diameter
+print(f'Circumference of the circle is {length_of_circle:.2f}')
+
+#3
+a = 38
+b = 18
+mean = (a+b)/ 2
+print(f'Mean of {a} and {b} is {mean}')
+
+#4
+num1 = 49
+num2 = 39
+total = num1 + num2
+product = num1 * num2
+num1_square = num1 ** 2
+num2_square = num2 ** 2
+print(f'Sum: {total}, Product: {product}')
+print(f'Square of {num1}: {num1_square}, Square of {num2}: {num2_square}')
